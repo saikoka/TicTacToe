aef70ad37801d3385610013646d4fde7ab4b461e
Sai Koka <saikoka.sk@gmail.com>
2022-12-17 14:39:13 -0500

    Fixed JS blocking DOM Rendering, fixed Diagonal win and tie game logic.

--- TicTacToe.js
+++ TicTacToe.js
@@ -29,16 +29,17 @@ const checkWinner = (row, col) =>{
     //Querying specific rol/col indicies for a potential winner. 
     const rowWin = boardArray[row][0] === (p1Turn ? "O" : "X") && boardArray[row][1] === (p1Turn ? "O" : "X") && boardArray[row][2] === (p1Turn ? "O" : "X");
     const colWin = boardArray[0][col] === (p1Turn ? "O" : "X") && boardArray[1][col] === (p1Turn ? "O" : "X") && boardArray[2][col] === (p1Turn ? "O" : "X");
-    const diagWin1 = boardArray[0][0] === (p1Turn ? "O" : "X") && boardArray[0][1] === (p1Turn ? "O" : "X") && boardArray[2][2] === (p1Turn ? "O" : "X");
-    const diagWin2 = boardArray[0][2] === (p1Turn ? "O" : "X") && boardArray[0][1] === (p1Turn ? "O" : "X") && boardArray[2][0] === (p1Turn ? "O" : "X");
+    const diagWin1 = boardArray[0][0] === (p1Turn ? "O" : "X") && boardArray[1][1] === (p1Turn ? "O" : "X") && boardArray[2][2] === (p1Turn ? "O" : "X");
+    const diagWin2 = boardArray[0][2] === (p1Turn ? "O" : "X") && boardArray[1][1] === (p1Turn ? "O" : "X") && boardArray[2][0] === (p1Turn ? "O" : "X");
 
-    if(rowWin || colWin || diagWin1 || diagWin2){
+    const winner = rowWin || colWin || diagWin1 || diagWin2;
+    if(winner){
         announceWinner();
     }
  
     
-    if(turnCount===8){
-        window.alert("TIE GAME!"); //TODO: Alert comes up before DOM is redrawn. 
+    if(!winner && turnCount===9){
+        setTimeout(()=>{window.alert("TIE GAME!"); }, 0);
     }else{
         p1Turn = !p1Turn;
     }
@@ -46,5 +47,5 @@ const checkWinner = (row, col) =>{
 
 const announceWinner = () =>{
     const winner = p1Turn ? "Player 1 won!" : "Player 2 Won";
-    window.alert(winner); //TODO: Alert comes up before DOM is redrawn to show cross or circle, fix ordering. 
+    setTimeout(()=>{window.alert(winner)}, 0); //TODO: Alert comes up before DOM is redrawn to show cross or circle, fix ordering. 
 }
